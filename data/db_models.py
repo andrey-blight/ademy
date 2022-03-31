9fa41841a7ff878f0d0ef3316000764c2373cd69
Kizhinov-Andrey <kzihinov05@gmail.com>
2022-03-31 17:41:11 +0300

    Add images

--- data/db_models.py
+++ data/db_models.py
@@ -16,6 +16,7 @@ class User(SqlAlchemyBase):
     age = Column(String(25), nullable=False)
     sex = Column(Integer, nullable=False)
     hashed_password = Column(String(200), nullable=False)
+    email = Column(String(25), unique=True)
     created_on = Column(DateTime, default=datetime.now)
     updated_on = Column(DateTime, default=datetime.now, onupdate=datetime.now)
     images = relationship("Image", back_populates="user")
@@ -24,11 +25,12 @@ class User(SqlAlchemyBase):
         CheckConstraint('sex IN (1, 2)', name='check_sex'),  # if sex is 1 - Male else sex is Female (ISO/IEC 5218)
     )
 
-    def __init__(self, name, surname, age, sex, password):
+    def __init__(self, name, surname, age, sex, password, email):
         self.name = name
         self.surname = surname
         self.age = age
         self.sex = sex
+        self.email = email
         self._set_password(password)
 
     def _set_password(self, password):
@@ -43,12 +45,11 @@ class Image(SqlAlchemyBase):
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('users.id'))
     image = Column(mysql.MEDIUMBLOB, nullable=False)
-    index = Column(Integer)
+    count = Column(Integer)
     user = relationship("User")
 
     __table_args__ = (
-        CheckConstraint('index <= 5', name='check_count_for_one_user'),
-        UniqueConstraint('user_id', 'image', name='unique_ssn')
+        CheckConstraint('count <= 5', name='check_count_for_one_user'),
     )
 
     def __init__(self, user_id, image):
@@ -58,4 +59,4 @@ class Image(SqlAlchemyBase):
 
     def set_index(self):
         session = create_session()
-        self.index = session.query(Image).where(self.user_id == Image.user_id).count() + 1
+        self.count = session.query(Image).where(self.user_id == Image.user_id).count() + 1
--- test_db.py
+++ test_db.py
@@ -2,18 +2,34 @@ from data.db_session import global_init, create_session
 from data.db_models import User, Image
 from PIL import Image as PILImage
 import io
+from sqlalchemy.exc import OperationalError
 
-global_init()
 
-for i in range(1, 7):
-    source = fr"test_images/{i}.jpg"
-    destination = fr"test_images/{i}.webp"
-    image = PILImage.open(source)  # Open image
-    b = io.BytesIO()
-    image.save(b, format="webp")  # Convert image to webp
-    im_bytes = b.getvalue()
-    if im_bytes.__sizeof__() < 55000:
-        session = create_session()
-        db_img = Image(user_id=1, image=im_bytes)
-        session.add(db_img)
-        session.commit()
+def create_users():
+    session = create_session()
+    u1 = User(name="Andrey", surname="Kizhinov", age=17, sex=1, password="12djfefd", email="kizhinov05@gmail.com")
+    u2 = User(name="Daria", surname="Lavrenteva", age=17, sex=2, password="eifavb", email="Daria@gmail.com")
+    u3 = User(name="Vadim", surname="Dragan", age=16, sex=1, password="weqifl", email="Vadim@gmail.com")
+    session.add_all([u1, u2, u3])
+    session.commit()
+
+
+def create_images():
+    for i in range(1, 8):
+        try:
+            source = fr"test_images/{i}.jpg"
+            image_jpg = PILImage.open(source)  # Open image
+            bite = io.BytesIO()
+            image_jpg.save(bite, format="webp")  # Convert image to webp
+            im_bytes = bite.getvalue()
+            session = create_session()
+            db_img = Image(user_id=3, image=im_bytes)
+            session.add(db_img)
+            session.commit()
+        except OperationalError as ex:
+            print(ex.args[0].split("'")[1])
+
+
+if __name__ == '__main__':
+    global_init()
+    create_images()
