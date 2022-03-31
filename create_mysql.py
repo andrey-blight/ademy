2134a31347c85ef8acc323e42f9fc2b051cd8f32
Andrey <Kizhinov05@gmail.com>
2022-03-31 19:37:13 +0300

    Add orm models for register user

--- create_mysql.py
+++ create_mysql.py
@@ -1,7 +1,6 @@
 import pymysql
 
-conn = pymysql.connect(host='127.0.0.1', user='root', passwd='Nbgfydf12!?',
-                       database='site_database')  # TODO change passowrd
+conn = pymysql.connect(host='127.0.0.1', user='root', passwd='Your_password!?')  # TODO change passowrd
 cur = conn.cursor()
 cur.execute("CREATE DATABASE site_database")
 cur.close()
