993fb76bc07b2b951a406dc3a7ec48ab025a4ba3
Kizhinov-Andrey <kzihinov05@gmail.com>
2022-04-26 19:18:15 +0300

    Add filling bd

--- test_db.py
+++ test_db.py
@@ -1,55 +1,42 @@
-import json
-import pprint
-
-import requests
-
-from Classes.SqlAlchemyDatabase import SqlAlchemyDatabase, SqlAlchemyBase
+from Classes.SqlAlchemyDatabase import SqlAlchemyDatabase
 from Data.Models.User import User
 from Data.Models.Image import Image
 from Data.Models.Interest import Interest
 from sqlalchemy.exc import OperationalError
+from faker import Faker
+import random
 
 
 def create_users():
     session = db.create_session()
-    u1 = User(name="Nasty", surname="Polykova", age=14, sex=2,
-              password="484365", email="NastyPol@gmail.com")
-    u2 = User(name="Daria", surname="Lavrenteva", age=17, sex=2,
-              password="eifavb", email="Daria@gmail.com")
-    u3 = User(name="Vadim", surname="Dragan", age=16, sex=1, password="weqifl",
-              email="Vadim@gmail.com")
-    session.add_all([u1, u2, u3])
+    faker = Faker()
+    users = []
+    for i in range(10):
+        user = User(name=faker.first_name(), surname=faker.last_name(),
+                    age=random.randint(16, 80), sex=random.randint(1, 2),
+                    password=faker.md5(raw_output=False), email=faker.email())
+        users.append(user)
+    session.add_all(users)
     session.commit()
 
 
 def create_interests():
     session = db.create_session()
-    u1 = session.query(User).get(1)
-    u2 = session.query(User).get(2)
-    u3 = session.query(User).get(3)
     i1 = Interest("sport")
     i2 = Interest("fishing")
     i3 = Interest("reading")
     session.add_all([i1, i2, i3])
-    u1.interests.append(i1)
-    u1.interests.append(i3)
-    u2.interests.append(i2)
-    u2.interests.append(i3)
-    u3.interests.append(i1)
     session.commit()
 
 
 def create_images():
-    for i in range(1, 6):
-        try:
-            source = fr"test_images/{i}.jpg"
-            session = db.create_session()
-            db_img = Image(user_id=3, image_href=source)
-            u3 = session.query(User).get(3)
-            u3.images.append(db_img)
-            session.commit()
-        except OperationalError as ex:
-            print(ex.args[0].split("'")[1])
+    session = db.create_session()
+    for i in range(1, 10 + 1):
+        user = session.query(User).get(i)
+        db_img = Image(user_id=i,
+                       image_href=r"static/user_images/test_image.jpg")
+        user.images.append(db_img)
+    session.commit()
 
 
 if __name__ == '__main__':
@@ -57,8 +44,3 @@ if __name__ == '__main__':
     create_users()
     create_images()
     create_interests()
-    # json_dict = {'name': 'Andrey', 'surname': 'Kizhinov', 'age': 18, 'sex': 1, 'password': 'ывакауцп6',
-    #              'email': 'kizhinov@gmail.com', 'interests': ['', '']}
-    # url = "http://localhost:8080/api/v1/users"
-    # user_json = requests.post(url, json=json_dict).json()
-    # print(user_json)
